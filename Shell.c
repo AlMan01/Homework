497ea72ecf5f4e386ef9fa334fac5bc6c077692e
AlMan01 <90648958+AlMan01@users.noreply.github.com>
2021-10-28 18:33:30 +0400

    Update Shell.c

--- Shell.c
+++ Shell.c
@@ -1,17 +1,24 @@
-#include <stdio.h>
-#include <sys/wait.h>
+#include <stdio.h> 
+#include <string.h> 
 #include <stdlib.h>
-#include <string.h>
 #include <unistd.h>
+#include <sys/wait.h>
+#include <fcntl.h>
+#include <sys/stat.h>
 
 #define BUFFER_SIZE 1024
 #define TOK_BUFFER_SIZE 100
 #define TOK_DELIM "\t\n"
 
+char* read_line();
+char** split_line(char* line);
+int execute(char** args);
+char* line;
+char** args;
+int status;
+
 void inf_loop() {
-	char* line;
-	char** args;
-	int status;
+	
 	do {
 		printf(">");
 		line = read_line();
@@ -23,14 +30,14 @@ void inf_loop() {
 }
 
 char* read_line() {
-	int BUFFER_SIZE, i = 0;
+  int i = 0;
 	char* buffer = malloc(sizeof(char) * BUFFER_SIZE);
 	int c;
 	if(!buffer)
 		printf("THE MEMORY IS NOT ALOCCATED\n");
-		exit();
+		exit(EXIT_FAILURE);
 	while(1) {
-		c = get_char();
+		c = getchar();
 		if(c == EOF || c == '\n') {
 			buffer[i] = '\0';
 			return buffer;
@@ -40,26 +47,27 @@ char* read_line() {
 		}
 		++i;
 		if(i >= BUFFER_SIZE){
-			BUFFER_SIZE += BUFFER_SIZE;
-			buffer = malloc(sizeof(char) * BUFFER_SIZE);
+			int BUFFER_SIZE_2 = 2*BUFFER_SIZE;
+			buffer = malloc(sizeof(char) * BUFFER_SIZE_2);
 			if(!buffer) {
 				printf("THE MEMORY IS NOT ALOCCATED\n");
-				exit();
+				exit(EXIT_FAILURE);
 			}
 		}
 	}
-
 }
 
-void split_line(char* line) {
+
+
+char** split_line(char* line) {
 	int buffer = TOK_BUFFER_SIZE, position = 0;
 	char** tokens = malloc(buffer * sizeof(char*));
 	char* token;
 	if(!tokens) {
 		printf("THE MEMORY IS NOT ALOCCATED\n");
-		exit();
+		exit(EXIT_FAILURE);
 	}
-	token strtok(line, TOK_DELIM);
+	token=strtok(line, TOK_DELIM);
 	while(token != NULL) {
 		tokens[position] = token;
 		position++;
@@ -69,13 +77,33 @@ void split_line(char* line) {
 			if(!tokens) {
 				printf("THE MEMORY IS NOT ALOCCATED\n");
 			}
-			exit();
+			exit(EXIT_FAILURE);
 		}
 		token = strtok(NULL, TOK_DELIM);
 	}
 	tokens[position] = NULL;
 	return tokens;
 }
+
+int execute(char **args) {
+    int rc = fork();
+    if (rc < 0) {
+      fprintf(stderr, "fork failed\n");
+      exit(1);
+    } else if (rc == 0) {
+      close(STDOUT_FILENO);
+      open("./p4.output", O_CREAT|O_WRONLY|O_TRUNC, S_IRWXU);
+
+      char *myargs[3];
+      myargs[0] = strdup("line"); 
+      myargs[1] = strdup("args"); 
+      myargs[2] = NULL; 
+      execvp(myargs[0], myargs); 
+    } else {
+      int rc_wait = wait(NULL);
+      }
+ return 0;
+ }
 
 int main(int argc, char* argv[]) {
 	inf_loop();
