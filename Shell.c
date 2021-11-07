56626ac1f3e8397fbb3a6cfa238fc8f93c400f8f
AlMan01 <90648958+AlMan01@users.noreply.github.com>
2021-11-07 13:17:37 +0400

    Update Shell.c

--- Shell.c
+++ Shell.c
@@ -11,10 +11,8 @@
 #define TOK_BUFFER_SIZE 100
 #define TOK_DELIM " "
 
-
-
 char* read_line() {
-  int buffer_size=BUFFER_SIZE;
+  int buffer_size = BUFFER_SIZE;
 	int i = 0;
 	char* buffer = malloc(sizeof(char) * BUFFER_SIZE);
 	int c; //for reading symbols
@@ -45,8 +43,8 @@ char* read_line() {
 }
 
 char** split_line(char* line) {
-	if(line==NULL){
-    printf("Error");
+	if(line == NULL){
+    printf("ERROR");
     return NULL;
   }
 	int tok_size = TOK_BUFFER_SIZE, position = 0;
@@ -61,8 +59,8 @@ char** split_line(char* line) {
 		tokens[position] = token;
 		position++;
 		if(position >= tok_size) {
-			tok_size +=100;
-			tokens = realloc(tokens, tok_size * sizeof(char * ));
+			tok_size += 100;
+			tokens = realloc(tokens, tok_size * sizeof(char *));
 			if(!tokens) {
 				printf("THE MEMORY IS NOT ALOCCATED\n");
 				exit(EXIT_FAILURE);
@@ -75,9 +73,7 @@ char** split_line(char* line) {
 }
 
 int execute(char ** args) {
-	pid_t cpid;
-  
-  
+  pid_t cpid;
   if (strcmp(args[0], "exit") == 0)
   {
   	return 0;
@@ -86,14 +82,15 @@ int execute(char ** args) {
   cpid = fork();
 
   if (cpid == 0) {
-    if (execvp(args[0], args) < 0){
+    if (execvp(args[0], args) < 0) {
       printf("dash: command not found: %s\n", args[0]);
       exit(EXIT_FAILURE);
     }  
 
-  } else if (cpid < 0){
+  } 
+  else if (cpid < 0) {
     printf( "Error forking");
-    }
+  }
   else {
     wait(&cpid);
   }
@@ -103,15 +100,15 @@ int execute(char ** args) {
 void inf_loop() {
   char* line;
   char** args;
-  int status=1;
+  int status = 1;
 	do {
 		printf(">");
-		line=read_line();
+		line = read_line();
 		args = split_line(line);
 		status = execute(args);
-    free(line);
-    free(args);
-	}while(status);
+    		free(line);
+    		free(args);
+	} while(status);
 }
 
 int main(int argc, char* argv[]) {
