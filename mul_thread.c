746963bbd0dcb65096e871e7410b70d34ee71cd4
AlMan01 <90648958+AlMan01@users.noreply.github.com>
2021-11-30 21:56:37 +0400

    Update mul_thread.c

--- mul_thread.c
+++ mul_thread.c
@@ -3,36 +3,43 @@
 #include <stdlib.h>
 
 #define N 5
-int result = 0;
+int arr[] =  {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15};
 
-void* reduce(void *(f_p)(void*), void *d_p) {
-	pthread_t threads[N];
-	for(int i = 0; i < N; ++i) {
-		pthread_t p;
-		pthread_create(&p, NULL, f_p, d_p + (i * 5));
-		threads[i] = p;
-	}
-	for(int i = 0; i < N; ++i) {
-		pthread_join(threads[i], NULL);
-	}
-
-	return (void*)&result;
+void* reduce(void (f_p)(void), void *d_p) {
+	pthread_t th[N];
+    int i;
+    for (i = 0; i < N; i++) {
+        int* a = malloc(sizeof(int));
+        *a = i * N;
+        if (pthread_create(&th[i], NULL, &f_p, a) != 0) {
+            perror("Failed to create thread");
+        }
+    }
+    int globalSum = 0;
+    for (i = 0; i < N; i++) {
+        int* r;
+        if (pthread_join(th[i], (void**) &r) != 0) {
+            perror("Failed to join thread");
+        }
+        globalSum += *r;
+        free(r);
+    }
+  printf("%d",globalSum);
+	return (void*)&globalSum;
 }
 
-void* f_p(void *arr) {
-	int* intArr = (int*)arr;
-	for(int i = 0; i < N; ++i) {
-		result += intArr[i];
-	}
-	return (void*)&result;
+void* f_p(void *arg) {
+	int index = (int)arg;
+    int sum = 0;
+    for (int j = 0; j < 5; j++) {
+        sum += arr[index + j];
+    }
+    printf("Local sum: %d\n", sum);
+    (int)arg = sum;
+    return arg;
 }
 
 int main(int argc, char** argv) {
-	const int size = 15;
-	int arr[] =  {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15};
-	int *res = (int*)reduce(f_p, arr);
-	printf("%d", *res);
+	reduce(f_p, arr);
 	return 0;
 }
-
-
